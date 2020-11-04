ff895b0d7243f9d3b4e53d91a3a1f5486eb7d5a5
Jonna Tuomisto <tuomisto.jonna@gmail.com>
2020-11-04 14:19:25 +0200

    Start with wall climbing
    Made climber, climber_hand and utility scripts for wall climbing. Changed some rocks to Climbing folder and gonna use them only for rockclimbing rocks. Added couple new materials for climbing rocks.

---
+++ VR-Devaus/Assets/Scripts/Climber.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climber : MonoBehaviour
+{
+    public float gravity = 45.0f;
+    public float sensitivity = 45.0f;
+
+    private Climber_Hand currentHand = null;
+    private CharacterController controller = null;
+    
+    
+    
+    private void CalculateMovement()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (currentHand)
+            movement += currentHand.Delta * sensitivity;
+        
+        if (movement == Vector3.zero)
+            movement.y -= gravity * Time.deltaTime;
+
+        controller.Move(movement * Time.deltaTime);
+    }
+
+    public void SetHand(Climber_Hand hand)
+    {
+        if(currentHand)
+            currentHand.ReleasePoint();
+        
+        currentHand = hand;
+    }
+
+    public void ClearHand()
+    {
+        currentHand = null;
+    }
+    
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+}
---
+++ VR-Devaus/Assets/Scripts/Climber_Hand.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climber_Hand : MonoBehaviour
+{
+    public Climber climber = null;
+    public OVRInput.Controller controller = OVRInput.Controller.None;
+
+    public Vector3 Delta {private set; get; } = Vector3.zero;
+
+    private Vector3 lastPosition = Vector3.zero;
+
+    private GameObject currentPoint = null;
+    public List<GameObject> contactPoints = new List<GameObject>();
+    
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One, controller))
+            GrapPoint();
+
+        if (OVRInput.GetUp(OVRInput.Button.One, controller))
+            ReleasePoint();
+    }
+
+    void FixedUpdate()
+    {
+        lastPosition = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Delta = lastPosition - transform.position;
+    }
+
+    private void GrapPoint()
+    {
+        currentPoint = Utility.GetNearest(transform.position, contactPoints);
+
+        if(currentPoint)
+        {
+            climber.SetHand(this);
+        }
+    }
+    
+    public void ReleasePoint()
+    {
+        if(currentPoint)
+        {
+            climber.ClearHand();
+        }
+
+        currentPoint = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AddPoint(other.gameObject);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        RemovePoint(other.gameObject);
+    }
+    
+    private void AddPoint(GameObject newObject)
+    {
+        if (newObject.CompareTag("ClimberPoint"))
+            contactPoints.Add(newObject);
+    }
+
+    private void RemovePoint(GameObject newObject)
+    {
+        if (newObject.CompareTag("ClimberPoint"))
+            contactPoints.Remove(newObject);
+    }
+
+}
